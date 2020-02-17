a04cd550368f02f0569000a9479bc4d9739922d6
Kabure <ferreira_master@hotmail.com>
2020-02-16 23:52:33 -0300

    layout updates

--- graphsutils.py
+++ graphsutils.py
@@ -72,8 +72,10 @@ def plot_dist_churn(df, col, binary='Churn'):
                          ))
 
     fig = go.Figure(data=[trace1, trace2, trace3], layout=layout)
-    fig.update_layout(title_x=.5, legend_orientation='h', height=450,
-                      legend=dict(x=.2, y=-.06))
+
+    fig.update_layout(title_x=.5, legend_orientation='h', 
+                      height=450, 
+                      legend=dict(x=.002, y=-.06))
     return fig
 
 def pie_norm(df, val1, val2, limit=15):
