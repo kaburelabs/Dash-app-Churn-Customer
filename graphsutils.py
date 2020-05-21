16911f96defbac640464a0dbfd2b5e4f0d4afa9e
Kabure <ferreira_master@hotmail.com>
2020-05-20 21:26:39 -0300

    layoutResponsive

--- graphsutils.py
+++ graphsutils.py
@@ -3,6 +3,7 @@ import plotly.graph_objs as go
 import dash_core_components as dcc
 import dash_html_components as html
 
+
 def plot_dist_churn2(df, col):
 
     tmp_attr = df[col].value_counts()
@@ -10,14 +11,14 @@ def plot_dist_churn2(df, col):
     trace1 = go.Bar(
         x=tmp_attr.sort_index().index,
         y=tmp_attr.sort_index().values,
-        #name='Yes_Churn',
-        opacity = 0.8, marker=dict(
+        # name='Yes_Churn',
+        opacity=0.8, marker=dict(
             color='seagreen',
-            line=dict(color='#000000',width=1)))
-    
-    layout = dict(title =  f'Distribution of {str(col)}',
-                  xaxis={'type':'category'}, 
-                  yaxis=dict(title= 'Count'))
+            line=dict(color='#000000', width=1)))
+
+    layout = dict(title=f'Distribution of {str(col)}',
+                  xaxis={'type': 'category'},
+                  yaxis=dict(title='Count'))
 
     fig = go.Figure(data=[trace1], layout=layout)
     fig.update_layout(title_x=.5, legend_orientation='h', height=450,
@@ -25,22 +26,24 @@ def plot_dist_churn2(df, col):
 
     return fig
 
+
 def plot_dist_churn(df, col, binary='Churn'):
     tmp_churn = df[df[binary] == 1]
     tmp_no_churn = df[df[binary] == 0]
-    tmp_attr = round(tmp_churn[col].value_counts().sort_index() / df[col].value_counts().sort_index(),2)*100
+    tmp_attr = round(tmp_churn[col].value_counts().sort_index(
+    ) / df[col].value_counts().sort_index(), 2)*100
     # print(f'Distribution of {col}: ')
     trace1 = go.Bar(
         x=tmp_churn[col].value_counts().sort_index().index,
         y=tmp_churn[col].value_counts().sort_index().values,
-        name='Churn',opacity = 0.8, marker=dict(
+        name='Churn', opacity=0.8, marker=dict(
             color='seagreen',
             line=dict(color='#000000', width=1)))
 
     trace2 = go.Bar(
         x=tmp_no_churn[col].value_counts().sort_index().index,
         y=tmp_no_churn[col].value_counts().sort_index().values,
-        name='No Churn', opacity = 0.8, 
+        name='No Churn', opacity=0.8,
         marker=dict(
             color='indianred',
             line=dict(color='#000000',
@@ -48,108 +51,111 @@ def plot_dist_churn(df, col, binary='Churn'):
         )
     )
 
-    trace3 =  go.Scatter(   
+    trace3 = go.Scatter(
         x=tmp_attr.sort_index().index,
         y=tmp_attr.sort_index().values,
-        yaxis = 'y2',
-        name='%Churn', opacity = 0.6, 
+        yaxis='y2',
+        name='%Churn', opacity=0.6,
         marker=dict(
             color='black',
             line=dict(color='#000000',
-                      width=2 )
+                      width=2)
         )
     )
-    
-    layout = dict(title =  f'Distribution of {str(col)} feature <br>by Churn Ratio ',
-              xaxis=dict(), 
-              yaxis=dict(title= 'Count'), 
-              yaxis2=dict(range= [0, 100], 
-                          overlaying= 'y', 
-                          anchor= 'x', 
-                          side= 'right',
-                          zeroline=False,
-                          showgrid= False, 
-                          title= 'Percentual Churn Ratio'
-                         ))
+
+    layout = dict(title=f'Distribution of {str(col)} feature <br>by Churn Ratio ',
+                  xaxis=dict(),
+                  yaxis=dict(title='Count'),
+                  yaxis2=dict(range=[0, 100],
+                              overlaying='y',
+                              anchor='x',
+                              side='right',
+                              zeroline=False,
+                              showgrid=False,
+                              title='Percentual Churn Ratio'
+                              ))
 
     fig = go.Figure(data=[trace1, trace2, trace3], layout=layout)
 
-    fig.update_layout(title_x=.5, legend_orientation='h', 
-                      height=450, 
+    fig.update_layout(title_x=.5, legend_orientation='h',
+                      height=450,
                       legend=dict(x=.002, y=-.06))
     return fig
+
 
 def pie_norm(df, val1, val2, limit=15):
     tmp = df.groupby(val1)[val2].sum().nlargest(limit).to_frame().reset_index()
     tmp = tmp.sort_values(val1)
 
-    trace1 = go.Pie(labels=tmp[val1], sort=False, 
-                    #textposition='outside', 
-                    #legendgroup='True',
-                    #textpositionsrc='outside',
-                    values=tmp[val2], name=str(val1), hole= .5, 
-                    hoverinfo="label+percent+name+value", 
-                    #showlegend=True,
+    trace1 = go.Pie(labels=tmp[val1], sort=False,
+                    # textposition='outside',
+                    # legendgroup='True',
+                    # textpositionsrc='outside',
+                    values=tmp[val2], name=str(val1), hole=.5,
+                    hoverinfo="label+percent+name+value",
+                    # showlegend=True,
                     #domain= {'x': [.52, 1]}
                     )
 
-    layout = dict(title={'text':str(f"{val2} Ratio of <br>{val1} by General")},
-                #         'xanchor':'center', 'yanchor':'top'},
-                #          height=350, font=dict(size=12), 
-                  annotations = [
+    layout = dict(title={'text': str(f"{val2} Ratio of <br>{val1} by General")},
+                  #         'xanchor':'center', 'yanchor':'top'},
+                  #          height=350, font=dict(size=12),
+                  annotations=[
                       dict(
-                          text='%General', 
+                          text='%General',
                           showarrow=False,
                           font=dict(size=15)
                       )
-        ], titlefont={'size':15}# sort=False
-        )
-    fig  = go.Figure(data=[trace1], layout=layout)
+    ], titlefont={'size': 15}  # sort=False
+    )
+    fig = go.Figure(data=[trace1], layout=layout)
 
-    fig.update_layout(title_x=.5, 
-                      legend_orientation='h', 
-                      legend=dict(x=.003, y=.01))
-    
-    fig['layout']['height'] = 380
-    fig['layout']['width'] = 350
-    
+    fig.update_layout(title_x=.5,
+                      legend_orientation='h', autosize=True,
+                      margin=dict(t=65, b=35, r=35, l=35))
+
+    #fig['layout']['height'] = 380
+    # fig['layout']['width'] = 350
+
     return fig
 
+
 def pie_churn(df, val1, val2, binary, limit=15):
-    
+
     if binary == 'Churn':
         bina = 1
     else:
         bina = 0
 
-    tmp = df[df['Churn'] == bina].groupby(val1)[val2].sum().nlargest(limit).to_frame().reset_index()
+    tmp = df[df['Churn'] == bina].groupby(
+        val1)[val2].sum().nlargest(limit).to_frame().reset_index()
 
     tmp = tmp.sort_values(val1)
 
     trace1 = go.Pie(labels=tmp[val1], sort=False,
-                    values=tmp[val2], name=f'{binary}', hole= .5, 
+                    values=tmp[val2], name=f'{binary}', hole=.5,
                     hoverinfo="label+percent+name+value", showlegend=True,
                     #domain= {'x': [0, .48]}
                     )
 
-    layout = dict(title={'text':str(f"{val2} Ratio of <br>{val1} by {binary}")}, 
-                        # 'xanchor':'center', 'yanchor':'top'},
-                        #  height=500, font=dict(size=15), 
-                  annotations = [
+    layout = dict(title={'text': str(f"{val2} Ratio of <br>{val1} by {binary}")},
+                  # 'xanchor':'center', 'yanchor':'top'},
+                  #  height=500, font=dict(size=15),
+                  annotations=[
                       dict(
-                    #      x=.20, y=.5,
-                          text=f'%{binary}', 
+                          #      x=.20, y=.5,
+                          text=f'%{binary}',
                           showarrow=False,
                           font=dict(size=15)
                       )
-        ], titlefont={'size':15})
+    ], titlefont={'size': 15})
 
-    fig  = go.Figure(data=[trace1], layout=layout)
-    fig.update_layout(title_x=.5, 
-                      legend_orientation='h', 
-                      legend=dict(x=.003, y=.01))
-    fig['layout']['height'] = 380
-    fig['layout']['width'] = 350
+    fig = go.Figure(data=[trace1], layout=layout)
+
+    fig.update_layout(title_x=.5,
+                      legend_orientation='h', autosize=True,
+                      margin=dict(t=65, b=35, r=35, l=35),)
+    # fig['layout']['height'] = 380
+    #fig['layout']['width'] = 350
 
     return fig
-
--- layouts.py
+++ layouts.py
@@ -2,60 +2,76 @@
 
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 
 theme = {'font-family': 'Raleway', 'background-color': '#787878'}
 
 
 def graph_1():
-    graph = html.Div([   
-            dcc.Graph(id='Graph1', 
-                      className="six columns", 
-                      #style={'border':'dotted'}
-                    ),
-            dcc.Graph(id='Graph4', 
-            #style={'border-style': 'dotted', }, 
-            className="six columns", )
-            ])
+    graph = dbc.Row([
+        dbc.Col(
+            dcc.Graph(id='Graph1',
+                      ),
+            width={"size": 12, "offset": 0},
+            md={"size": 10, "offset": 1},
+            lg={"size": 6, "offset": 0}),
+        dbc.Col(
+            dcc.Graph(id='Graph4',
+                      ),
+            width={"size": 12, "offset": 0},
+            md={"size": 10, "offset": 1},
+            lg={"size": 6, "offset": 0}),
+    ])
     return [graph]
 
 
 def graph2_3():
-    graph_2 = html.Div([
-                dcc.Graph(id='Graph2',
-                        )], className="four columns", style={'margin':'0'})
-    graph_5 = html.Div([
-                dcc.Graph(id='Graph5',
-                        )], className="four columns", style={'margin':'0'})
-    graph_3 = html.Div([   
-                dcc.Graph(id='Graph3',
-                        )], className="four columns", style={'margin':'0'})
+    graph_2 = dbc.Col(html.Div(
+        dcc.Graph(id='Graph2',
+                  )),
+        width={"size": 12, "offset": 0},
+        md={"size": 6, "offset": 3},
+        lg={"size": 4, "offset": 0})
+    # className="col-10 offset-0 col-md-6 offset-md-0 col-lg-4 offset-lg-0")
+    graph_5 = dbc.Col(html.Div(
+        dcc.Graph(id='Graph5',
+                  )),
+        width={"size": 12, "offset": 0},
+        md={"size": 6, "offset": 0},
+        lg={"size": 4, "offset": 0})
+    graph_3 = dbc.Col(html.Div(
+        dcc.Graph(id='Graph3',
+                  )),
+        width={"size": 12, "offset": 0},
+        md={"size": 6, "offset": 0},
+        lg={"size": 4, "offset": 0})
 
     # Return of the graphs in all the row
-    return [graph_2, graph_3, graph_5]
+    return dbc.Row([graph_2, graph_3, graph_5])
 
 
 def create_header(some_string):
     header_style = {
-        'background-color':'#584b42',
+        'background-color': '#584b42',
         'padding': '1.5rem',
-        'display':'inline-block',
-        'width':'100%'
-        
-       # 'border-style': 'dotted'
+        'display': 'inline-block',
+        'width': '100%'
+
+        # 'border-style': 'dotted'
     }
     logo_trich = html.Img(
-                    src='/assets/fundo_transp-b.png',
-                    className='three columns',
-                    style={
-                        'height': 'auto',
-                        'width': '140px', # 'padding': 1
-                        'float': 'right', #'position': 'relative'
-                        'margin-right': '66px', #'border-style': 'dotted'
-                        'display':'inline-block'})
+        src='/assets/fundo_transp-b.png',
+        className='three columns',
+        style={
+            'height': 'auto',
+            'width': '140px',  # 'padding': 1
+            'float': 'right',  # 'position': 'relative'
+            'margin-right': '66px',  # 'border-style': 'dotted'
+            'display': 'inline-block'})
 
     title = html.H1(children=some_string, className='eight columns',
-                    style={'margin':'0 0 0 36px',
-                           'color':'#ffffff', 'font-size':'35px'})
+                    style={'margin': '0 0 0 36px',
+                           'color': '#ffffff', 'font-size': '35px'})
 
     header = html.Header(html.Div([title, logo_trich]), style=header_style)
 
@@ -66,23 +82,23 @@ def create_footer():
     p = html.P(
         children=[
             html.Span('Developed By: '),
-            html.A('trich.ai | Data Intelligence Solutions', 
-                   style={'text-decoration':'none', 'color':'#ffffff'},
+            html.A('trich.ai | Data Intelligence Solutions',
+                   style={'text-decoration': 'none', 'color': '#ffffff'},
                    href='https://trich.ai', target='_blank')
-        ], style={'float':'right', 'margin-top':'8px', 
-                  'font-size':'18px', 'color':'#ffffff' }
+        ], style={'float': 'right', 'margin-top': '8px',
+                  'font-size': '18px', 'color': '#ffffff'}
     )
 
-    span_style = {'horizontal-align': 'right', 
-                  'padding-left': '1rem', 
-                  'font-size':'15px', 
-                  'vertical-align':'middle'}
+    span_style = {'horizontal-align': 'right',
+                  'padding-left': '1rem',
+                  'font-size': '15px',
+                  'vertical-align': 'middle'}
 
     kaggle = html.A(
         children=[
             html.I([], className='fab fa-kaggle'),
             html.Span('Kaggle', style=span_style)
-        ], style={'text-decoration': 'none', 'color':'#ffffff', 'margin-right':'20px'},
+        ], style={'text-decoration': 'none', 'color': '#ffffff', 'margin-right': '20px'},
         href="https://www.kaggle.com/kabure/kernels",
         target='_blank')
 
@@ -90,31 +106,35 @@ def create_footer():
         children=[
             html.I([], className='fab fa-python'),
             html.Span('Dash Plotly', style=span_style)
-        ], style={'text-decoration': 'none', 'color':'#ffffff', 'margin-right':'20px'},
+        ], style={'text-decoration': 'none', 'color': '#ffffff', 'margin-right': '20px'},
         href='https://plot.ly/dash/', target='_blank')
 
     font_awesome = html.A(
         children=[
-            html.I([], className='fa fa-font-awesome'),
+            html.I([], className='fab fa-font-awesome'),
             html.Span('Font Awesome', style=span_style)
-        ], style={'text-decoration': 'none', 'color':'#ffffff', 'margin-right':'20px'},
+        ], style={'text-decoration': 'none', 'color': '#ffffff', 'margin-right': '20px'},
         href='http://fontawesome.io/', target='_blank')
 
     datatables = html.A(
         children=[
-            html.I([], className='fa fa-github'),
+            html.I([], className='fab fa-github'),
             html.Span('trich.ai\n Github', style=span_style)
-        ], style={'text-decoration': 'none', 'color':'#ffffff', 'margin-right':'20px'},
+        ], style={'text-decoration': 'none', 'color': '#ffffff', 'margin-right': '20px'},
         href='https://github.com/kaburelabs/', target='_blank')
 
     ul1 = html.Div(
         children=[
-            html.Li(mapbox, style={'display':'inline-block', 'color':'#ffffff'}),
-            html.Li(font_awesome, style={'display':'inline-block', 'color':'#ffffff'}),
-            html.Li(datatables, style={'display':'inline-block', 'color':'#ffffff'}),
-            html.Li(kaggle, style={'display':'inline-block', 'color':'#ffffff'}),
+            html.Li(mapbox, style={
+                    'display': 'inline-block', 'color': '#ffffff'}),
+            html.Li(font_awesome, style={
+                    'display': 'inline-block', 'color': '#ffffff'}),
+            html.Li(datatables, style={
+                    'display': 'inline-block', 'color': '#ffffff'}),
+            html.Li(kaggle, style={
+                    'display': 'inline-block', 'color': '#ffffff'}),
         ],
-        style={'list-style-type': 'none', 'font-size':'30px'},
+        style={'list-style-type': 'none', 'font-size': '30px'},
     )
 
     hashtags = 'plotly,dash,trich.ai'
@@ -122,11 +142,12 @@ def create_footer():
     twitter_href = 'https://twitter.com/intent/tweet?hashtags={}&text={}'\
         .format(hashtags, tweet)
     twitter = html.A(
-        children=html.I(children=[], className='fa fa-twitter'),
+        children=html.I(children=[], className='fab fa-twitter'),
         title='Tweet me!', href=twitter_href, target='_blank')
 
     github = html.A(
-        children=html.I(children=[], className='fa fa-github', style={'color':'black'}),
+        children=html.I(children=[], className='fab fa-github',
+                        style={'color': 'black'}),
         title='Repo on GitHub',
         href='https://github.com/kaburelabs/Dash-app-Churn-Customer', target='_blank')
 
@@ -142,7 +163,7 @@ def create_footer():
             'position': 'fixed',
             'right': '1.5rem',
             'bottom': '75px',
-            'font-size':'60px'
+            'font-size': '60px'
         }
     )
     div = html.Div([p, ul1, ul2])
@@ -150,41 +171,47 @@ def create_footer():
     footer_style = {
         'font-size': '2.2rem',
         'background-color': '#584b42',
-        #'padding': '2.5rem',
-        'margin-top': '3rem', 
-        'display':'inline-block', 'padding':'16px 32px 8px'
+        # 'padding': '2.5rem',
+        'margin-top': '3rem',
+        'display': 'inline-block', 'padding': '16px 32px 8px'
     }
     footer = html.Footer(div, style=footer_style, className='twelve columns')
     return footer
 
 
 def header_logo():
-    h1_title = html.H1(
-                    children='Churn Customer Prediction',
-                    #className="eight columns", 
-                    style={#'border-style': 'dotted', 
-                           'margin-top':20,
-                           #'float':'left',
-                           #'margin-left': 100,
-                           'font-size':'50px'
-                            #'display':'inline-block'
-                            })
-    subtitle = html.Div(dcc.Markdown('''
-      **Churn rate**, when applied to a customer base, refers to the proportion of contractual customers or subscribers who leave a supplier during a given time period. It is a possible indicator of customer dissatisfaction, cheaper and/or better offers from the competition, more successful sales and/or marketing by the competition, or reasons having to do with the customer life cycle.     
- '''))
+    h1_title = html.Div(
+        children='Churn Customer Prediction',
+        className="font-xl bold bottom16 margin-auto",
+    )
+    subtitle = html.Div(html.Div([html.Span("Churn rate ", className="bold"),
+                                  'when applied to a customer base, refers to the proportion of contractual\
+             customers or subscribers who leave a supplier during a given time period. \
+                 It is a possible indicator of customer dissatisfaction, cheaper and/or \
+                     better offers from the competition, more successful sales and/or\
+                          marketing by the competition, or reasons having to do with the customer life cycle.'], className="font-md margin-auto"))
     return [h1_title, subtitle]
-    
-## When talking about subscribers or customers, sometimes the expression **"survival rate"** is used to mean 1 minus the churn rate. For example, for a group of subscribers, an annual churn rate of 25 percent is the same as an annual survival rate of 75 percent. Both imply a customer lifetime of four years. I.e., a customer lifetime can be calculated as the inverse of that customer's predicted churn rate. For a group or segment of customers, their customer life (or tenure) is the inverse of their aggregate churn rate. Gompertz distribution models of distribution of customer life times can therefore also predict a distribution of churn rates.
+
+# When talking about subscribers or customers, sometimes the expression **"survival rate"** is used to mean 1 minus the churn rate. For example, for a group of subscribers, an annual churn rate of 25 percent is the same as an annual survival rate of 75 percent. Both imply a customer lifetime of four years. I.e., a customer lifetime can be calculated as the inverse of that customer's predicted churn rate. For a group or segment of customers, their customer life (or tenure) is the inverse of their aggregate churn rate. Gompertz distribution models of distribution of customer life times can therefore also predict a distribution of churn rates.
+
 
 def paragraphs():
-    div = html.H1("Revenue Churn", style={'width':'85%', 
-                                          'margin':'0 auto',  
-                                          'text-align':'center','padding':'24px 0px 10px'})
-    paragra = html.P(dcc.Markdown("  **Revenue churn** is the monetary amount of recurring revenue lost in a period divided by the total revenue at the beginning of the period. Revenue churn is commonly used in Software as a Service (SaaS) and other business models that rely on recurring revenue models."), 
-    style={'width':'85%', 'margin':'0 auto', 
-                    'padding-bottom':'24px', 
-                           # 'margin':'24px 0'
-                            })
+    div = html.Div("Revenue Churn",
+
+                   className="font-xl bold bottom16 margin-auto",
+                   # style={'width': '85%',
+                   #                                       'margin': '0 auto',
+                   #                                       'text-align': 'center', 'padding': '24px 0px 10px'}
+                   )
+    paragra = html.Div(html.Div([html.Span("Revenue churn ", className="bold"), "is the monetary amount of \
+        recurring revenue lost in a period divided by the total revenue at the beginning of \
+            the period. Revenue churn is commonly used in Software as a Service (SaaS) \
+                and other business models that rely on recurring revenue models."]),
+                       className="font-md bottom32 margin-auto"
+                       #    style={'width': '85%', 'margin': '0 auto',
+                       #           'padding-bottom': '24px',
+                       #           # 'margin':'24px 0'
+                       #           }
+                       )
 
     return [div, paragra]
-
